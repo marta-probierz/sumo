d8783174a05666078fcbe35ad8dcd9863b9ee3e0
Marta Probierz <marta.anna.probierz@gmail.com>
2023-03-27 08:12:32 +0200

    add intro-header and the second section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
+import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { TermsComponent } from './terms/terms.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'terms', component: TermsComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
