e4e9571bed03d0fddf4a147cc270c70586f47865
Marta Probierz <marta.anna.probierz@gmail.com>
2023-03-31 16:23:45 +0200

    add form logic

--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
 })
-export class TermsComponent {}
+export class TermsComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  scrollToTop(): void {
+    return this.document.body.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'start',
+    });
+  }
+}
