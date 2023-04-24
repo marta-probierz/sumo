750ada1cfa3551c7b473ac6a4a888481006c0aa5
Marta Probierz <marta.anna.probierz@gmail.com>
2023-04-24 17:12:46 +0200

    add the second section of capabilities

--- src/app/capabilities/capabilities.component.ts
+++ src/app/capabilities/capabilities.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CapabilitiesService } from '../services/capabilities.service';
+
 @Component({
   selector: 'app-capabilities',
   templateUrl: './capabilities.component.html',
-  styleUrls: ['./capabilities.component.scss']
+  styleUrls: ['./capabilities.component.scss'],
 })
 export class CapabilitiesComponent implements OnInit {
+  capabilitiesData: any;
 
-  constructor() { }
+  constructor(private capabilitiesService: CapabilitiesService) {}
 
   ngOnInit(): void {
+    this.capabilitiesService.getCapabilities().subscribe((res) => {
+      this.capabilitiesData = res;
+    });
   }
-
 }
